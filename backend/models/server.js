3f4ec48fe1cdcde1d50339df18c7ad98e685b603
martinrotela <martinrotela47@gmail.com>
2022-08-31 03:40:09 -0300

    connect rds database

--- backend/models/server.js
+++ backend/models/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const db = require('../database/connection');
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.dbConnection();
     }
 
     listen() {
         this.app.listen(this.port, () => {
             console.log(`Servidor funcionando en el puerto ${this.port}`);
         });
+    }
+
+    async dbConnection() {
+        try {
+            await db.sync();
+
+            console.log('Database online');
+        } catch (error) {
+            console.log(error);
+            throw new Error('An unexpected error ocurred');
+        }
     }
 }
 

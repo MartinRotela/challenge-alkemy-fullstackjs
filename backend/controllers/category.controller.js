6d2bbab08369b4e015bb0500e67c24770700191c
martinrotela <martinrotela47@gmail.com>
2022-09-10 06:11:18 -0300

    fix category controller

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -2,22 +2,31 @@ const Category = require('../models/category');
 const Entry = require('../models/Entry');
 
 const getCategories = async (req, res) => {
-    const categories = await Category.findAll();
-
-    res.json({
-        categories,
-    });
+    try {
+        const categories = await Category.findAll();
+        res.json({
+            categories,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'An unexpected error ocurred' });
+    }
 };
 
 const getCategoryByIdAndHisEntries = async (req, res) => {
-    const { id } = req.params;
-    const { uid } = req.body;
+    try {
+        const { id } = req.params;
+        const { uid } = req.body;
 
-    const category = await Category.findByPk(id, {
-        include: { model: Entry, as: 'entries', where: { UserId: uid } },
-    });
+        const category = await Category.findByPk(id, {
+            include: { model: Entry, as: 'entries', where: { UserId: uid } },
+        });
 
-    res.json(category);
+        res.json(category);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'An unexpected error ocurred' });
+    }
 };
 
 module.exports = { getCategories, getCategoryByIdAndHisEntries };

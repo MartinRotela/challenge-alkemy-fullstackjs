de73b20f560fd3ccdf428c5c189d99152fe72c9c
martinrotela <martinrotela47@gmail.com>
2022-09-15 22:50:51 -0300

    delete features

--- frontend/src/components/router/AppRouter.jsx
+++ frontend/src/components/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { LoginScreen } from '../auth/LoginScreen';
 import { RegisterScreen } from '../auth/RegisterScreen';
+import { HomeScreen } from '../home/HomeScreen';
 
 export const AppRouter = () => {
     return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
             <Routes>
                 <Route path="/login" element={<LoginScreen />} />
                 <Route path="/register" element={<RegisterScreen />} />
+                <Route path="/" element={<HomeScreen />} />
                 <Route path="*" element={<Navigate replace to="/login" />} />
             </Routes>
         </BrowserRouter>

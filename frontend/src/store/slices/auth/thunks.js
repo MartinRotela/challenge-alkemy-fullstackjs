37170b66426da42c3dfd6929be79bdc69261b6c0
martinrotela <martinrotela47@gmail.com>
2022-09-15 22:08:25 -0300

    add register thunk

--- frontend/src/store/slices/auth/thunks.js
+++ frontend/src/store/slices/auth/thunks.js
@@ -23,3 +23,25 @@ export const startLogin = (email, password) => {
         }
     };
 };
+export const startRegister = (name, email, password) => {
+    return async (dispatch) => {
+        try {
+            dispatch(startLoadingUser());
+            const { data } = await financesApi.post('/users/new', {
+                name,
+                email,
+                password,
+            });
+            localStorage.setItem('token', data.token);
+            dispatch(setUser(data));
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const err = error.response.data;
+                const { msg } = err;
+                dispatch(setErrorMessage(msg));
+            } else {
+                throw new Error('An unexpected error ocurred');
+            }
+        }
+    };
+};

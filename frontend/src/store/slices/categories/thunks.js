21a9fcc573b2e52ac74aaf5593a211ec194324a8
martinrotela <martinrotela47@gmail.com>
2022-09-16 21:37:42 -0300

    add start setting categories thunk

---
+++ frontend/src/store/slices/categories/thunks.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { financesApi } from '../../../api/financesApi';
+import {
+    setCategories,
+    setErrorMessage,
+    startLoadingCategories,
+} from '../categoriesSlice';
+
+export const startSettingCategories = (email, password) => {
+    return async (dispatch) => {
+        try {
+            dispatch(startLoadingCategories());
+            const token = localStorage.getItem('token');
+            const { data } = await financesApi.get('/categories/', {
+                headers: { 'x-token': token },
+            });
+            const { categories } = data;
+            console.log(categories);
+            dispatch(setCategories(categories));
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const err = error.response.data;
+                const { msg } = err;
+                dispatch(setErrorMessage(msg));
+            } else {
+                throw new Error('An unexpected error ocurred');
+            }
+        }
+    };
+};

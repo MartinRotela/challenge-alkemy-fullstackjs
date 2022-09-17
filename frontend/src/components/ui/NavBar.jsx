c2bc090f321341740d05e91ed90ed15758b54cff
martinrotela <martinrotela47@gmail.com>
2022-09-16 22:18:23 -0300

    fix navbar links

--- frontend/src/components/ui/NavBar.jsx
+++ frontend/src/components/ui/NavBar.jsx
@@ -1,14 +1,21 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
     return (
         <>
             <Navbar bg="dark" variant="dark" className="mb-4">
                 <Container>
-                    <Navbar.Brand href="#home">Finances!</Navbar.Brand>
+                    <Navbar.Brand as={NavLink} to="/home">
+                        Finances!
+                    </Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Categories</Nav.Link>
+                        <Nav.Link as={NavLink} to="/home">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/categories">
+                            Categories
+                        </Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>

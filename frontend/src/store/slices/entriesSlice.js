fb6972bad4baf0a71eed590529e89707c622b603
martinrotela <martinrotela47@gmail.com>
2022-09-16 21:28:27 -0300

    fix entries slice name

--- frontend/src/store/slices/entriesSlice.js
+++ frontend/src/store/slices/entriesSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const entriesSlice = createSlice({
-    name: 'auth',
+    name: 'entries',
     initialState: {
         entries: [],
         isLoading: false,

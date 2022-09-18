2e23f8617ec308a0cf4aef52db75c34f231b6d6b
martinrotela <martinrotela47@gmail.com>
2022-09-18 20:52:45 -0300

    add new entry thunks

--- frontend/src/store/slices/entries/thunks.js
+++ frontend/src/store/slices/entries/thunks.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import { financesApi } from '../../../api/financesApi';
 import {
+    addNewEntry,
+    deleteEntry,
     setBalance,
     setEntries,
     setErrorMessage,
     startLoadingEntries,
+    updateEntry,
 } from '../entriesSlice';
 
 export const startSettingEntries = () => {
@@ -25,6 +28,71 @@ export const startSettingEntries = () => {
             const { entries } = data;
             dispatch(setEntries(entries));
             dispatch(setBalance(balance));
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const err = error.response.data;
+                const { msg } = err;
+                dispatch(setErrorMessage(msg));
+            } else {
+                throw new Error('An unexpected error ocurred');
+            }
+        }
+    };
+};
+
+export const startUpdateEntry = (id, newEntry) => {
+    return async (dispatch) => {
+        try {
+            const token = localStorage.getItem('token');
+            const { data } = await financesApi.put(`/entries/${id}`, newEntry, {
+                headers: { 'x-token': token },
+            });
+            dispatch(updateEntry(data));
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const err = error.response.data;
+                const { msg } = err;
+                dispatch(setErrorMessage(msg));
+            } else {
+                throw new Error('An unexpected error ocurred');
+            }
+        }
+    };
+};
+
+export const startDeleteEntry = (id) => {
+    return async (dispatch) => {
+        try {
+            const token = localStorage.getItem('token');
+            await financesApi.delete(`/entries/${id}`, {
+                headers: { 'x-token': token },
+            });
+            dispatch(deleteEntry(id));
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const err = error.response.data;
+                const { msg } = err;
+                dispatch(setErrorMessage(msg));
+            } else {
+                throw new Error('An unexpected error ocurred');
+            }
+        }
+    };
+};
+
+export const startAddNewEntry = (entry) => {
+    return async (dispatch) => {
+        try {
+            const token = localStorage.getItem('token');
+            const { data } = await financesApi.post(
+                `/entries/new`,
+                { ...entry },
+                {
+                    headers: { 'x-token': token },
+                }
+            );
+            const { newEntry } = data;
+            dispatch(addNewEntry(newEntry));
         } catch (error) {
             if (axios.isAxiosError(error)) {
                 const err = error.response.data;

73e4b30c2f1867199193434c161ec6084b6dea08
martinrotela <martinrotela47@gmail.com>
2022-09-18 20:55:34 -0300

    add  modal to entry item and fix missing elemets

--- frontend/src/components/entries/EntryItem.jsx
+++ frontend/src/components/entries/EntryItem.jsx
@@ -1,11 +1,26 @@
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../store/slices/modalSlice';
 
-export const EntryItem = ({ description, type, amount, createdAt }) => {
+export const EntryItem = ({
+    id,
+    description,
+    type,
+    amount,
+    createdAt,
+    updatedAt,
+}) => {
     const value = type === 'expense' ? '-' : '+';
     const date = moment(createdAt).format('MMMM Do, YYYY');
+    const dispatch = useDispatch();
+    const activeEvent = { description, amount, type, id, createdAt, updatedAt };
+
+    const openModModal = () => {
+        dispatch(openModal(activeEvent));
+    };
     return (
         <>
-            <tr>
+            <tr onDoubleClick={openModModal}>
                 <td>
                     {value} ${amount}
                 </td>

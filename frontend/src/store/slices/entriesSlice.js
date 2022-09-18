e8ae546238b5c3e79a0168444d1797aeb97cb0e1
martinrotela <martinrotela47@gmail.com>
2022-09-18 20:52:23 -0300

    add new actions to entries slice

--- frontend/src/store/slices/entriesSlice.js
+++ frontend/src/store/slices/entriesSlice.js
@@ -24,8 +24,28 @@ export const entriesSlice = createSlice({
             state.isLoading = false;
             state.errorMessage = action.payload;
         },
+        updateEntry: (state, action) => {
+            state.entries = state.entries.map((e) =>
+                e.id === action.payload.id ? action.payload : e
+            );
+        },
+        deleteEntry: (state, action) => {
+            state.entries = state.entries.filter(
+                (e) => e.id !== action.payload
+            );
+        },
+        addNewEntry: (state, action) => {
+            state.entries = [action.payload, ...state.entries];
+        },
     },
 });
 
-export const { startLoadingEntries, setEntries, setBalance, setErrorMessage } =
-    entriesSlice.actions;
+export const {
+    startLoadingEntries,
+    setEntries,
+    setBalance,
+    setErrorMessage,
+    addNewEntry,
+    updateEntry,
+    deleteEntry,
+} = entriesSlice.actions;

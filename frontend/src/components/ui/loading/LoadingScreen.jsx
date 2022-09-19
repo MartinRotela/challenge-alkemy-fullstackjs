19a5b2a0d70402434e7e3ecffb16f4004b2c0608
martinrotela <martinrotela47@gmail.com>
2022-09-18 23:04:01 -0300

    add loading screen

---
+++ frontend/src/components/ui/loading/LoadingScreen.jsx
@@ -0,0 +1,27 @@
+import { Col, Container, Row, Spinner, Stack } from 'react-bootstrap';
+
+export const LoadingScreen = () => {
+    return (
+        <Container>
+            <Row className="justify-content-md-center">
+                <Col
+                    md="auto"
+                    className="position-fixed h-75  d-flex align-items-center"
+                >
+                    <Stack
+                        direction="horizontal"
+                        gap={3}
+                        className="d-flex justify-content-center"
+                    >
+                        <h2 className="mr-2">Loading</h2>
+                        <Spinner
+                            animation="border"
+                            variant="dark"
+                            className="ml-2"
+                        />
+                    </Stack>
+                </Col>
+            </Row>
+        </Container>
+    );
+};

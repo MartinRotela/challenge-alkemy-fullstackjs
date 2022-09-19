4171c9253500b6444b89aad48b462323c0cd5677
martinrotela <martinrotela47@gmail.com>
2022-09-18 23:08:45 -0300

    add loading screen

--- frontend/src/components/home/HomeScreen.jsx
+++ frontend/src/components/home/HomeScreen.jsx
@@ -1,36 +1,46 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import { EntriesList } from '../entries/EntriesList';
-import { NavBar } from '../ui/NavBar';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { startSettingEntries } from '../../store/slices/entries/thunks';
+import {
+    startSetBalance,
+    startSettingEntries,
+} from '../../store/slices/entries/thunks';
 import { startSettingCategories } from '../../store/slices/categories/thunks';
 import { AppModal } from '../ui/modal/AppModal';
 import { AddNewButton } from '../ui/buttons/AddNewButton';
+import { LoadingScreen } from '../ui/loading/LoadingScreen';
 
 export const HomeScreen = () => {
     const dispatch = useDispatch();
-    const { balance } = useSelector((state) => state.entries);
+    const { balance, isLoading } = useSelector((state) => state.entries);
+
+    const loading = useMemo(() => isLoading, [isLoading]);
 
     useEffect(() => {
         dispatch(startSettingEntries());
         dispatch(startSettingCategories());
+        dispatch(startSetBalance());
     }, [dispatch]);
 
     return (
         <>
-            <Container>
-                <AppModal />
-                <Row className="d.flex justify-content-center">
-                    <Col xl={6} className="d.flex justify-content-center">
-                        <h1 className="text-center font-weight-bold mb-5">
-                            ${balance.toFixed(2)}
-                        </h1>
-                        <EntriesList />
-                    </Col>
-                </Row>
-                <AddNewButton />
-            </Container>
+            {loading ? (
+                <LoadingScreen />
+            ) : (
+                <Container>
+                    <AppModal />
+                    <Row className="d.flex justify-content-center">
+                        <Col xl={6} className="d.flex justify-content-center">
+                            <h1 className="text-center font-weight-bold mb-5">
+                                ${balance.toFixed(2)}
+                            </h1>
+                            <EntriesList />
+                        </Col>
+                    </Row>
+                    <AddNewButton />
+                </Container>
+            )}
         </>
     );
 };

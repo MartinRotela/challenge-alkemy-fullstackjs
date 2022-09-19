dcb88c3f1d401e7a3e85c5d28a4d0ebe89d23348
martinrotela <martinrotela47@gmail.com>
2022-09-18 23:10:44 -0300

    add pagination

--- frontend/src/components/entries/EntriesList.jsx
+++ frontend/src/components/entries/EntriesList.jsx
@@ -1,31 +1,50 @@
-import { useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import { useState } from 'react';
+import { ButtonGroup, Table, ToggleButton } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { startSetBalance } from '../../store/slices/entries/thunks';
 import { EntryItem } from './EntryItem';
 
 export const EntriesList = () => {
     const { entries, isLoading } = useSelector((state) => state.entries);
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(startSetBalance());
-    }, [dispatch, entries]);
+    const [currentPage, setCurrentPage] = useState(0);
 
+    const filteredEntries = entries.slice(currentPage, currentPage + 10);
+
+    const nextPage = () => {
+        if (currentPage * 10 < entries.length) {
+            setCurrentPage(currentPage + 10);
+        }
+    };
+    const prevPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 10);
+        }
+    };
     return (
-        <Table>
-            <thead>
-                <tr>
-                    <th>Amount</th>
-                    <th>Description</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                {entries.map((entry) => (
-                    <EntryItem key={entry.id} {...entry} />
-                ))}
-            </tbody>
-        </Table>
+        <>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Amount</th>
+                        <th>Description</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredEntries.map((entry) => (
+                        <EntryItem key={entry.id} {...entry} />
+                    ))}
+                </tbody>
+            </Table>
+            <ButtonGroup>
+                <ToggleButton onClick={prevPage} variant="dark">
+                    Previous
+                </ToggleButton>
+                <ToggleButton onClick={nextPage} variant="dark">
+                    Next
+                </ToggleButton>
+            </ButtonGroup>
+        </>
     );
 };
